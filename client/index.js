41814c9cf8878d1bbf1eeea63d6b235d2d5cdee4
Eliot <14015887+eliotwycoff@users.noreply.github.com>
2022-04-30 16:23:19 +0800

    ready for submission

--- client/index.js
+++ client/index.js
@@ -2,33 +2,112 @@ import "./index.scss";
 
 const server = "http://localhost:3042";
 
+const closeTransferPanel = () => {
+  clearAmount();
+  clearRecipient();
+  updateSignatureInstructions();
+  clearSignatureR();
+  clearSignatureS();
+  clearError();
+  document.getElementById("send").style.display = "none";
+};
+
 document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
   if(value === "") {
     document.getElementById("balance").innerHTML = 0;
+    closeTransferPanel();
     return;
   }
 
   fetch(`${server}/balance/${value}`).then((response) => {
     return response.json();
   }).then(({ balance }) => {
+    console.log(balance);
     document.getElementById("balance").innerHTML = balance;
+
+    if (parseFloat(balance) > 0) {
+      document.getElementById("send").style.display = "block";
+    } else {
+      closeTransferPanel();
+    }
   });
 });
+
+const clearUsername = () => {
+  document.getElementById("exchange-address").value = '';
+  document.getElementById("send").style.display = "none";
+};
+
+const clearAmount = () => {
+  document.getElementById("send-amount").value = '';
+};
+
+const clearRecipient = () => {
+  document.getElementById("recipient").value = '';
+};
+
+const updateSignatureInstructions = () => {
+  document.getElementById("sample-amount").innerHTML = document.getElementById("send-amount").value;
+  document.getElementById("sample-recipient").innerHTML = document.getElementById("recipient").value;
+}
+
+const clearSignatureR = () => {
+  document.getElementById("signature-r").value = '';
+}
+
+const clearSignatureS = () => {
+  document.getElementById("signature-s").value = '';
+};
+
+const clearError = () => {
+  document.getElementById("error").innerHTML = '';
+};
+
+const setError = (errorMessage) => {
+  document.getElementById("error").innerHTML = errorMessage;
+};
+
+clearUsername();
+clearAmount();
+clearRecipient();
+updateSignatureInstructions();
+clearSignatureR();
+clearSignatureS();
+
+document.getElementById("send-amount").addEventListener('input', updateSignatureInstructions);
+document.getElementById("recipient").addEventListener('input', updateSignatureInstructions);
+
+document.getElementById("copy-button").addEventListener('click', () => {
+  navigator.clipboard.writeText(document.getElementById("code").textContent);
+});
+
+document.getElementById("refresh-r").addEventListener('click', clearSignatureR);
+document.getElementById("refresh-s").addEventListener('click', clearSignatureS);
 
 document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
+  const signatureR = document.getElementById("signature-r").value;
+  const signatureS = document.getElementById("signature-s").value;
 
   const body = JSON.stringify({
-    sender, amount, recipient
+    sender, amount, recipient, signatureR, signatureS
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
 
   fetch(request, { headers: { 'Content-Type': 'application/json' }}).then(response => {
     return response.json();
-  }).then(({ balance }) => {
+  }).then(({ balance, error }) => {
     document.getElementById("balance").innerHTML = balance;
+    if (error == '') {
+      clearAmount();
+      clearRecipient();
+      updateSignatureInstructions();
+      clearError();
+    } else {
+      setError(error);
+    }
   });
 });
--- server/index.js
+++ server/index.js
@@ -1,30 +1,100 @@
+const EC = require('elliptic').ec;
+const SHA256 = require('crypto-js/sha256');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
+
+console.log(`\n`);
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+// Create some accounts with balances.
+const accounts = {
+  'Alice': {
+    balance: 100
+  },
+  'Bob': {
+    balance: 50
+  },
+  'Satoshi': {
+    balance: 75
+  }
+};
+
+// Generate elliptic-curve public and private key pairs for each account.
+const ec = new EC('secp256k1');
+
+for (let user in accounts) {
+  console.log(`Generating an account for ${user}...`);
+  console.log(`Balance: ${accounts[user].balance} ETH`);
+  const keyPair = ec.genKeyPair();
+  
+  accounts[user].publicKey = `0x${keyPair.getPublic().encode('hex').slice(2)}`;
+  accounts[user].publicX = keyPair.getPublic().x.toString(16);
+  accounts[user].publicY = keyPair.getPublic().y.toString(16);
+  console.log(`Public Key: ${accounts[user].publicKey}`);
+  
+  // Show the private key, but don't save it anywhere.
+  console.log(`Private Key: ${keyPair.getPrivate().toString(16)}`);
+  console.log(`***Please save your private key. It has been deleted from the server and will not be shown again!***\n`);
 }
 
-app.get('/balance/:address', (req, res) => {
-  const {address} = req.params;
-  const balance = balances[address] || 0;
+app.get('/balance/:user', (req, res) => {
+  const {user} = req.params;
+  const balance = accounts.hasOwnProperty(user) ? accounts[user].balance : 0;
   res.send({ balance });
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+  const {sender, recipient, amount, signatureR, signatureS} = req.body;
+  let error = '';
+
+  // Only process transactions if public key information is available for the sender.
+  if (accounts[sender].publicX != null && accounts[sender].publicY != null) {
+    // Check if the signature matches the public key.
+    const message = `{ "amount": ${amount}, "recipient": "${recipient}" }`;
+    const messageHash = SHA256(message).toString();
+
+    const publicKey = {
+      x: accounts[sender].publicX,
+      y: accounts[sender].publicY
+    };
+
+    const key = ec.keyFromPublic(publicKey, 'hex');
+
+    const signature = {
+      r: signatureR,
+      s: signatureS
+    };
+
+    if (key.verify(messageHash, signature)) {
+      accounts[sender].balance -= parseFloat(amount);
+
+      if (accounts.hasOwnProperty(recipient)) {
+        accounts[recipient].balance += parseFloat(amount);
+      } else {
+        accounts[recipient] = { 
+          balance: parseFloat(amount),
+          publicKey: null,
+          publicX: null,
+          publicY: null
+        };
+      }
+    } else {
+      error = "Verification Failed!";
+    }
+  } else {
+    error = "No public key is available for this user.";
+  }
+
+  res.send({ 
+    balance: accounts[sender].balance,
+    error: error
+   });
 });
 
 app.listen(port, () => {
